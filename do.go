8d06a0be9911c9858f07ad5a63dbf82685aa7188
Abdelrahman Taman <taman9333@gmail.com>
2024-06-20 16:16:18 +0300

    Handle cases where the recovered value r is not of type error

--- do.go
+++ do.go
@@ -1,12 +1,21 @@
 package mo
 
+import (
+	"errors"
+	"fmt"
+)
+
 // Do executes a function within a monadic context, capturing any errors that occur.
 // If the function executes successfully, its result is wrapped in a successful Result.
 // If the function panics (indicating a failure), the panic is caught and converted into an error Result.
 func Do[T any](fn func() T) (result Result[T]) {
 	defer func() {
 		if r := recover(); r != nil {
-			result = Err[T](r.(error))
+			if err, ok := r.(error); ok {
+				result = Err[T](err)
+			} else {
+				result = Err[T](errors.New(fmt.Sprint(r)))
+			}
 		}
 	}()
 	return Ok(fn())
