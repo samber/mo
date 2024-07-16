ef7a827b54634067f4503675656e33284f49a814
mtsoltan <mt@soltan.rs>
2024-07-16 13:05:55 +0900

    In 1.18, convertAssign has no way to use it...

--- option.go
+++ option.go
@@ -297,17 +297,7 @@ func (o *Option[T]) Scan(src any) error {
 		}
 	}
 
-	// we try to convertAssign values that we can't directly assign because ConvertValue
-	// will return immediately for v that is already a Value, even if it is a different
-	// Value type than the one we expect here.
-	var st sql.Null[T]
-	if err := st.Scan(src); err == nil {
-		o.isPresent = true
-		o.value = st.V
-		return nil
-	}
-
-	return fmt.Errorf("failed to scan Option[T]")
+	return o.scanConvertValue(src)
 }
 
 // Value implements the driver Valuer interface.
